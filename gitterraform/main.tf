cb1407ee1c13ac5e9c89b08660386dcf410ed74f
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-17 14:37:19 +0900

    Update main.tf

--- gitterraform/main.tf
+++ gitterraform/main.tf
@@ -5,3 +5,14 @@ module "AWS" {
   vpc_prefix = "STG"
   vpc_cidr = "10.0.0.0/16"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "hgc-tfstate-bucket" 
+    key            = "state/terraform.tfstate"
+    region         = "ap-northeast-2"              
+    dynamodb_table = "terraform-lock"         
+    encrypt        = true                   
+    versioning     = true                  
+  }
+}
