12e0d338e164ed934526841212b60615d49190a7
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-17 12:57:22 +0900

    Update s3.tf

--- gitterraform/AWS/s3.tf
+++ gitterraform/AWS/s3.tf
@@ -10,10 +10,15 @@ resource "aws_s3_bucket" "bucket" {
 
 # S3 bucket for backend
 resource "aws_s3_bucket" "tfstate" {
-  bucket = "tf101-jupiter-apne2-tfstate"
+  bucket = "hgc-tfstate-bucket"
 
   versioning {
-    enabled = true # Prevent from deleting tfstate file
+    enabled = true
+  }
+
+  tags = {
+    Name = "hgc-tfstate-bucket"
+    Environment = "${var.vpc_prefix}"
   }
 }
 
