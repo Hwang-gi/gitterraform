095aefa73f492d4f1b8e6bdb2290b40857bf0d42
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-18 13:45:29 +0900

    Update main.tf

--- gitterraform/main.tf
+++ gitterraform/main.tf
@@ -5,6 +5,3 @@ module "AWS" {
   vpc_prefix = "STG"
   vpc_cidr = "10.0.0.0/16"
 }
-
-AWS_ACCESS_KEY_ID     = "AKIAWAA66RBQLG5H7QVZ"
-AWS_SECRET_ACCESS_KEY = "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"
