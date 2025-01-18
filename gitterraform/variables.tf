1d51b007582f80ae16b4bb8a84331574caf26d1d
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-18 13:45:15 +0900

    Delete gitterraform/variables.tf

--- gitterraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS_ACCESS_KEY_ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS_SECRET_ACCESS_KEY"
-  type        = string
-}
