d4fadb9a07cadec7af084013e4cba7ea4edefe05
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-20 13:54:11 +0900

    Update main.tf

--- gitterraform/main.tf
+++ gitterraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-northeast-2"
+  region = var.AWS_REGION
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
@@ -16,7 +16,7 @@ provider "aws" {
 module "AWS" {
   source = "./AWS"
 
-  region = "ap-northeast-2"
+  region = var.AWS_REGION
   vpc_prefix = "STG"
   vpc_cidr = "10.0.0.0/16"
 }
