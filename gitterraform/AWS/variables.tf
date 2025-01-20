f3e3778c1c61a7727fd1e732c1c0b3ac6e6a8b2a
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-20 13:43:51 +0900

    Update variables.tf

--- gitterraform/AWS/variables.tf
+++ gitterraform/AWS/variables.tf
@@ -2,18 +2,6 @@ variable "region" {
   type = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  description = "Sensitive Access Key"
-  type        = string
-  default     = "AKIAWAA66RBQLG5H7QVZ"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "Sensitive Secret Access Key"
-  type        = string
-  default     = "vpBsKmkznXGTT5YzXluHuAiIU6W0dSB/m0K1Ezrg"
-}
-
 variable "account_id" {
   type = string
   default = "412381775968"
