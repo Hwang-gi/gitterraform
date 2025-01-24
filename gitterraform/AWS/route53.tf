f048fba84c4ccc20a1d21f15ebfbfdf9077fbfdb
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-24 21:12:34 +0900

    Update route53.tf

--- gitterraform/AWS/route53.tf
+++ gitterraform/AWS/route53.tf
@@ -1,19 +1,3 @@
 resource "aws_route53_zone" "main" {
   name = "gitchang.store"
 }
-
-resource "aws_route53_zone" "zone" {
-  name = "${lower(var.vpc_prefix)}.${aws_route53_zone.main.name}"
-
-  tags = {
-    Environment = var.vpc_prefix
-  }
-}
-
-resource "aws_route53_record" "A_record" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "${lower(var.vpc_prefix)}.gitchang.store"
-  type    = "A"
-  ttl     = "30"
-  records = aws_route53_zone.zone.name_servers
-}
