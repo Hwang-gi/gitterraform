c09c115a71865c2dcb16eab8e237dcb9022cc149
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-30 16:38:43 +0900

    Update default.tf

--- gitterraform/AWS/default.tf
+++ gitterraform/AWS/default.tf
@@ -5,31 +5,3 @@ resource "aws_vpc" "mainvpc" {
     Name = "DEFAULT-VPC"
   }
 }
-
-# Create a new network ACL or use an existing one
-resource "aws_network_acl" "main" {
-  vpc_id = aws_vpc.mainvpc.id
-
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-}
-
-resource "aws_network_acl_association" "example" {
-  network_acl_id = aws_network_acl.main.id
-  subnet_id      = aws_subnet.example.id
-}
