471c8f6dd6110f7cbc18863c89c153dde3dfd581
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-30 10:02:41 +0900

    Update variables.tf

--- gitterraform/variables.tf
+++ gitterraform/variables.tf
@@ -1,6 +1,12 @@
+# Terraform Cloud에서 정의한 variable
+variable "AWS_REGION" {
+  type = string
+}
+
 variable "AWS_ACCESS_KEY_ID" {
   type = string
 }
+
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
