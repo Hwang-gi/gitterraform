12bed0a975b07f458fe5e7bfa962d4b5f95a5cf9
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-30 10:01:27 +0900

    Update main.tf

--- gitterraform/main.tf
+++ gitterraform/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  region = TF_VAR_AWS_REGION
-  access_key = TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key = TF_VAR_AWS_SECRET_ACCESS_KEY
+  region = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "AWS" {
