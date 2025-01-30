7a0262a6244a402d983ceb6674dabcfd3c2894b0
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-31 08:59:54 +0900

    Update and rename helm.tf to kubernetes.tf

--- gitterraform/AWS/kubernetes.tf
+++ gitterraform/AWS/kubernetes.tf
@@ -1,5 +1,17 @@
-data "aws_eks_cluster_auth" "eks_cluster" {
+data "aws_eks_cluster" "cluster" {
   name = aws_eks_cluster.eks_cluster.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+provider "kubernetes" {
+  version                = "1.31.0"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
 }
 
 provider "helm" {
