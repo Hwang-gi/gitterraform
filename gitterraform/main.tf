dffd3f2707aa62a7e2222b390bd245e826685b4e
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-31 09:32:47 +0900

    Update main.tf

--- gitterraform/main.tf
+++ gitterraform/main.tf
@@ -1,12 +1,17 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = >= 2.0"
+    }
+    kubectl = {
+      source = "bnu0/kubectl"
+      version = ">= 1.7.0"
     }
   }
 }
