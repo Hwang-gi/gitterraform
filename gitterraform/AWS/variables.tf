a81b0c36e27894d7ac0580f77dc8d7d678b5f691
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-01-31 09:34:11 +0900

    Update variables.tf

--- gitterraform/AWS/variables.tf
+++ gitterraform/AWS/variables.tf
@@ -57,3 +57,15 @@ variable "argocd_chart" {
     version    = "5.46.6"
   }  
 } 
+
+variable "alb_chart" {
+  type        = map(string)
+  description = "AWS Load Balancer Controller chart"
+  default = {
+    name       = "aws-load-balancer-controller"
+    namespace  = "kube-system"
+    repository = "https://aws.github.io/eks-charts"
+    chart      = "aws-load-balancer-controller"
+    version    = "1.5.5"
+  }
+}
