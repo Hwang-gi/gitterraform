a0ef1cab57e682758b376f9e0571a7ca4ddd6bb7
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-02-08 18:47:11 +0900

    Update iam-eks-main.tf

--- aws/modules/iam/iam-eks-main.tf
+++ aws/modules/iam/iam-eks-main.tf
@@ -6,16 +6,11 @@ resource "aws_iam_role" "eks_role" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "eks.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-        }
-      ]
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
   ]
 }
