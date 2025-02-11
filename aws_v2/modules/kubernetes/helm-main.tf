d1adef2f27f40ffa7ff84ef7a4cdc04fe9658215
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-02-11 14:55:08 +0900

    Update helm-main.tf

--- aws_v2/modules/kubernetes/helm-main.tf
+++ aws_v2/modules/kubernetes/helm-main.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "default" {
-  name = var.eks_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = var.eks_name
-}
-
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.default.endpoint
