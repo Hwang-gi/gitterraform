a922d2d3c53315f4ab2c38f20b7dbce3b597069e
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-02-11 17:07:19 +0900

    Update kubernetes-main.tf

--- aws_v2/modules/eks/kubernetes-main.tf
+++ aws_v2/modules/eks/kubernetes-main.tf
@@ -1,3 +1,9 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.cluster_ca_certificate)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
