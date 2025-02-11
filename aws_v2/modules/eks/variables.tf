3665c2aae49a03a7949d3f3a11e78713bf2b250e
Hwang-gi <154039941+Hwang-gi@users.noreply.github.com>
2025-02-11 16:32:43 +0900

    Update variables.tf

--- aws_v2/modules/eks/variables.tf
+++ aws_v2/modules/eks/variables.tf
@@ -79,7 +79,7 @@ variable "alb_chart" {
     namespace  = "kube-system"
     repository = "https://aws.github.io/eks-charts"
     chart      = "aws-load-balancer-controller"
-    version    = "1.6.0"
+    version    = "2.11.0"
   }
 }
 
@@ -91,7 +91,7 @@ variable "cluster_autoscaler_chart" {
     namespace = "kube-system"
     repository = "https://kubernetes.github.io/autoscaler"
     chart = "cluster-autoscaler"
-    version = "latest-version"
+    version = "9.45.0"
   }
 }
 
@@ -103,7 +103,7 @@ variable "efs_csi_driver_chart" {
     namespace = "kube-system"
     repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
     chart = "aws-efs-csi-driver"
-    version = "latest-version"
+    version = "3.1.6"
   }
 }
 
@@ -115,7 +115,7 @@ variable "prometheus_chart" {
     namespace = "monitoring"
     repository = "https://prometheus-community.github.io/helm-charts"
     chart = "prometheus"
-    version = "latest-version"
+    version = "27.3.0"
   }
 }
 
@@ -127,6 +127,6 @@ variable "grafana_chart" {
     namespace = "monitoring"
     repository = "https://grafana.github.io/helm-charts"
     chart = "grafana"
-    version = "latest-version"
+    version = "8.9.0"
   }
 }
